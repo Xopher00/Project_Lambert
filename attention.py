0f1483f946e2206996a84124cba6552a2c605c31
Xopher00 <pricekr1221@gmail.com>
2026-03-08 22:06:27 +0200

    multi head hopeless broken starting over

--- attention.py
+++ attention.py
@@ -18,7 +18,7 @@ class Attention(Embed):
 
     def _step(self, q, temp):
         J   = self.Attend(q, self.emb, temp)
-        raw = self.SoftMax(J, temp, axis=0)
+        raw = self.SoftMax(J, temp, axis=0).squeeze()
         allowed   = self.Residuate(raw[:, None], self.emb.T, temp).squeeze()
         corrected = self.SmoothMin((raw, self.Join(allowed[None,:], self.emb, temp).squeeze()), temp, axis=0)
         return corrected, raw
@@ -38,29 +38,3 @@ class Attention(Embed):
         mask = scores >= scores.max() - self.fp.eps
         return np.where(mask)[0]
     
-
-class MultiHead(Attention):
-    def __init__(self, emb, head_cols, temp=1.0, eps=1e-3, max_iters=100):
-        self.head_cols = head_cols
-        self.heads = [Attention(emb[:, cols], temp=temp, eps=eps, max_iters=max_iters)
-                      for cols in head_cols]
-        self.active_heads = None  # in __init__
-        super().__init__(emb, temp=temp, eps=eps, max_iters=max_iters)
-
-    def _step(self, q, temp):
-        corrected = np.zeros_like(q)
-        raw = np.zeros_like(q)
-        for head, cols in zip(self.heads, self.head_cols):
-            if not np.any(q[cols]):
-                continue
-            c, r = head._step(q[cols], temp)
-            corrected[cols] = c
-            raw[cols] = r
-        return corrected, raw
-    
-    def scores(self, state):
-        active = self.active_heads
-        head_scores = [self.Join(state[None, cols], head.emb.T, self.fp.temp).squeeze()
-                    for i, (head, cols) in enumerate(zip(self.heads, self.head_cols))
-                    if (active is None or i in active) and len(cols) > 0]
-        return self.SmoothMin(tuple(head_scores), self.fp.temp, axis=0)
